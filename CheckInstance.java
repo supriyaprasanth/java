b43b967b96b76c8f02f2fb827ca76d6342f9a56b
supriyaprasanth <supriya.p@inapp.com>
2018-08-17 11:02:21 +0530

    more use of intsnaceof

--- CheckInstance.java
+++ CheckInstance.java
@@ -7,24 +7,36 @@ class Derived extends Base {}
 
 public class CheckInstance {
 	public static void main(String args[]){
+		Base base = new Base();
 		Derived derived = new Derived() ;
-		
+		Derived d=null;
+	 
 		if(derived instanceof Derived){
 			System.out.println("derived is an insatnce of Derived Class");
 		}else{
 			System.out.println("not an instance of Derived Class");
 		}
-		
 		if(derived instanceof Base){
 			System.out.println("is an instance of Base class");
 		}else{
 			System.out.println("is not an instance of Base class");
 		}
-		
 		if(derived instanceof Object){
 			System.out.println("is an instance of Object class");
 		}else{
 			System.out.println("is not an instance of Object Class");
+		}
+		
+		if(d instanceof Derived){
+			System.out.println("d is an instance of Derived class");
+		}else{
+			System.out.println("d is not an instance of Derived class");
+		}
+		
+		if(base instanceof Derived){
+			System.out.println("base is an instance of Derived Class");
+		}else{
+			System.out.println("base is not an instance of Derived Class");
 		}
 	}
 	
